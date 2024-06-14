26fbe550e6b7a52df73fba7f2b906a071c3e1662
Renan Santos <renan.santos@hipe.cc>
2024-06-14 08:54:49 -0300

    changed admin font-family

--- resources/views/admin/layout/layout.php
+++ resources/views/admin/layout/layout.php
@@ -6,6 +6,9 @@
     <title><?= $title ?? 'Admin' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/resources/css/admin_style.css">
 </head>
 
